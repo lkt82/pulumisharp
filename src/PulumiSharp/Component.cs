735969b7d7f950f3e3fa35e5aa066947d78f4fcd
Lars Kjær Thomsen <citybilen@gmail.com>
2023-09-10 14:31:27 +0200

    Added component class

---
+++ src/PulumiSharp/Component.cs
@@ -0,0 +1,35 @@
+﻿using Pulumi;
+
+namespace PulumiSharp;
+
+public abstract class Component<TType> : ComponentResource
+{
+    protected string Name => GetResourceName();
+    protected string Type => GetResourceType();
+
+    protected Component(string name, ComponentResourceOptions? options = null) : base(typeof(TType).Namespace != null ? typeof(TType).Namespace!.ToLower().Replace(".",":")+":"+ typeof(TType).Name : typeof(TType).Name, name, options)
+    {
+
+    }
+}
+
+public abstract class Component<TType,TArgs> : Component<TType>
+{
+    protected TArgs Args { get; init; }
+
+    protected Component(string name, TArgs args, ComponentResourceOptions? options = null) : base(name, options)
+    {
+        Args = args;
+    }
+}
+
+
+public abstract class Component<TType, TArgs, TConfig> : Component<TType, TArgs>
+{
+    protected TConfig Config { get; init; }
+
+    protected Component(string name, TArgs args, ComponentResourceOptions? options = null) : base(name, args, options)
+    {
+        Config = new Config(Type).RequireObject<TConfig>(name);
+    }
+}
